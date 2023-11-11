365c92abb64b543b6cf96094a16fc9caf3c7e00d
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-11 12:07:15 +0530

    final commit

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import admin from 'firebase-admin';
-import serviceAccount from './react-js-blog-website-f2d81-firebase-adminsdk-buh39-364a00ca85.json';
-import {getAuth} from 'firebase-admin/auth';
+import admin from "firebase-admin";
+import serviceAccount from "./react-js-blog-website-f2d81-firebase-adminsdk-buh39-364a00ca85.json";
+import { getAuth } from "firebase-admin/auth";
 
+import User from "./Schema/User.js";
+import { formatDatatoSend, generateUsername } from "./utils/assets.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 app.use(cors());
@@ -24,23 +26,44 @@ mongoose.connect(process.env.MONGO_URL, {
   autoIndex: true,
 });
 
-
 // Importing user routes from user.routes.js
 import userRouter from "./routes/user.routes.js";
-import User from "./Schema/User.js";
 app.use("/", userRouter);
 
+app.post("/google-auth", async (req, res) => {
+  try {
+    const { access_token } = req.body;
+    const decodedUser = await getAuth().verifyIdToken(access_token);
 
-app.post("/googel-auth",async(req,res)=>{
-  let {access_token} = req.body;
-  getAuth().verifyIdToken(access_token).then(async(decodedUser)=>{
-    let {email,name,picture} = decodedUser;
-    picture = picture.replace("s96-c","s384-c");
-    let user = await User.findOne({"personalInfo.email":email}).select("personalInfo.fullname personalInfo.fullname")
-  })
-})
+    let { email, name, picture } = decodedUser;
+    picture = picture.replace("s96-c", "s384-c");
+
+    let user = await User.findOne({ "personalInfo.email": email }).select(
+      "personalInfo.fullname personalInfo.username personalInfo.profileImg google_auth"
+    );
+
+    if (user) {
+      if (!user.google_auth) {
+        return res.status(403).json({
+          error:
+            "This email was signed up without Google. Please log in with a password to access the account",
+        });
+      }
+    } else {
+      let username = await generateUsername(email);
+      user = new User({
+        personalInfo: { fullname: name, email, profileImg: picture, username },
+        google_auth: true,
+      });
+
+      await user.save();
+      return res.status(200).json(formatDatatoSend(user));
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT -> ${PORT}`);
 });
-
