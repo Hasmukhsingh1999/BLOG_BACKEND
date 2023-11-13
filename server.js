fc182722b42d1fb257c2fe65d01823959148d22a
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-13 14:35:11 +0530

    optimised

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import morgan from "morgan";
 import multer from "multer";
 import path from "path";
-import cloudinary from 'cloudinary';
+
 
 dotenv.config();
 
@@ -39,47 +39,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-
-// const cloudinary = require('cloudinary').v2;
-
-
-// Configure Cloudinary with your credentials
-// cloudinary.config({
-//   cloud_name: process.env.CLOUD_NAME,
-//   api_key: process.env.API_KEY,
-//   api_secret: process.env.API_SECRET,
-// });
-
-// const storage = multer.memoryStorage(); 
-// const upload = multer({ storage: storage });
-
-// app.post('/api/upload', upload.single('file'), async (req, res) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).json({ error: 'No file uploaded.' });
-//     }
-
-//     const result = await cloudinary.uploader.upload_stream(
-//       (error, result) => {
-//         if (error) {
-//           console.error('Error uploading to Cloudinary:', error);
-//           return res.status(500).json({ error: 'Failed to upload file.' });
-//         }
-        
-
-//         res.status(200).json({
-//           success: true,
-//           message: 'File has been uploaded.',
-          
-//         });
-//       }
-//     ).end(req.file.buffer);
-
-//   } catch (error) {
-//     console.error('Error uploading to Cloudinary:', error);
-//     res.status(500).json({ error: 'Failed to upload file.' });
-//   }
-// });
 
 
 
