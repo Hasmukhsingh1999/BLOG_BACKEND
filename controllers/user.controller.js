897323beb4afc7da044b66a85aa0a090cfecc139
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-14 12:13:18 +0530

    final commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -99,33 +99,34 @@ export const createBlog = async (req, res, next) => {
   try {
     let authorId = req.user;
     let { title, banner, content, des, tags, draft } = req.body;
-
     if (!title.length) {
       return res
         .status(405)
         .json({ error: "You must provide a title to publish the blog" });
     }
-    if (!des.length || des.length > 200) {
-      return res.status(403).json({
-        error: "You must provide a blog description under 200 characters.",
-      });
-    }
+    if (!draft) {
+      if (!des.length || des.length > 200) {
+        return res.status(403).json({
+          error: "You must provide a blog description under 200 characters.",
+        });
+      }
 
-    if (!banner.length) {
-      return res
-        .status(403)
-        .json({ error: "You must provide a blog banner to publish it" });
-    }
-    if (!content.blocks.length) {
-      return res
-        .status(403)
-        .json({ error: "There must be some blog content to publish it." });
-    }
+      if (!banner.length) {
+        return res
+          .status(403)
+          .json({ error: "You must provide a blog banner to publish it" });
+      }
+      if (!content.blocks.length) {
+        return res
+          .status(403)
+          .json({ error: "There must be some blog content to publish it." });
+      }
 
-    if (!tags.length || tags.length > 10) {
-      return res.status(403).json({
-        error: "Provide tags in order to publish the blog, Maximum 10",
-      });
+      if (!tags.length || tags.length > 10) {
+        return res.status(403).json({
+          error: "Provide tags in order to publish the blog, Maximum 10",
+        });
+      }
     }
 
     tags = tags.map((tag) => tag.toLowerCase());
